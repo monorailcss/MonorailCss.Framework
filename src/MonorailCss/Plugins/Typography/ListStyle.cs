fd26000c45911b60ea53db1a2b2fa04ccb0434c5
Phil Scott <phil.scott@gmail.com>
2022-04-17 15:33:40 -0400

    Adding default content and list style

---
+++ src/MonorailCss/Plugins/Typography/ListStyle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Immutable;
+
+namespace MonorailCss.Plugins.Typography;
+
+/// <summary>
+/// The list-style plugin.
+/// </summary>
+public class ListStyle : BaseUtilityPlugin
+{
+    /// <inheritdoc />
+    protected override string Property => "list-style-type";
+
+    /// <inheritdoc />
+    protected override ImmutableDictionary<string, string> GetUtilities()
+    {
+        return new Dictionary<string, string>()
+        {
+            { "list-none", "none" }, { "list-disc", "disc" }, { "list-decimal", "decimal" },
+        }.ToImmutableDictionary();
+    }
+}
+
+/// <summary>
+/// The list-style-position plugin.
+/// </summary>
+public class ListStylePosition : BaseUtilityPlugin
+{
+    /// <inheritdoc />
+    protected override string Property => "list-style-position";
+
+    /// <inheritdoc />
+    protected override ImmutableDictionary<string, string> GetUtilities()
+    {
+        return new Dictionary<string, string>() { { "list-inside", "inside" }, { "list-outside", "outside" }, }
+            .ToImmutableDictionary();
+    }
+}
