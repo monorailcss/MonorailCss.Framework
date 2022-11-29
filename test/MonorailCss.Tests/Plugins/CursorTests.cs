222f04d8c3882ee266b4897bf48e65f14f0c1c03
Phil Scott <phil.scott@gmail.com>
2022-11-29 14:23:25 -0500

    adding cursor

---
+++ test/MonorailCss.Tests/Plugins/CursorTests.cs
@@ -0,0 +1,17 @@
+﻿namespace MonorailCss.Tests.Plugins;
+
+public class CursorTests
+{
+
+    [Fact]
+    public void Cursor_works()
+    {
+        var framework = new CssFramework(new CssFrameworkSettings {CssResetOverride = string.Empty});
+        var result = framework.Process(new[] { "cursor-help" });
+        result.ShouldBeCss(@"
+.cursor-help {
+  cursor:help;
+}
+");
+    }
+}
